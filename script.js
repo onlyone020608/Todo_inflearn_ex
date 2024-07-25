79c28ff4ea849804e2dea1f2bec847462d0b401d
onlyone020608 <onlyone0608@hufs.ac.kr>
2024-07-25 13:57:02 +0900

    할 일 추가/전체삭제 구현

---
+++ script.js
@@ -0,0 +1,35 @@
+let todoInput = document.querySelector("#todo-input");
+
+const createTodo = function () {
+  const todoList = document.querySelector("#todo-list");
+  const newLi = document.createElement("li");
+  const newSpan = document.createElement("span");
+  const newBtn = document.createElement("button");
+
+  newBtn.addEventListener("click", () => {
+    newLi.classList.toggle("complete");
+  });
+
+  newLi.addEventListener("dbclick", () => {
+    newLi.remove();
+  });
+
+  newSpan.textContent = todoInput.value;
+  newLi.appendChild(newBtn);
+  newLi.appendChild(newSpan);
+  todoList.appendChild(newLi);
+  todoInput.value = "";
+};
+
+const keyCodeCheck = function (e) {
+  if (e.key === "Enter" && todoInput.value) {
+    createTodo();
+  }
+};
+
+const deleteAll = function () {
+  const liList = document.querySelectorAll("li");
+  for (let i = 0; i < liList.length; i++) {
+    liList[i].remove();
+  }
+};
