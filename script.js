bfddd366da568ab7e1bd256f7227cd97b55e84ae
onlyone020608 <onlyone0608@hufs.ac.kr>
2024-07-27 13:35:12 +0900

    local storage 저장/ 다시 들어가도 todolist 유지 코드 추가

--- script.js
+++ script.js
@@ -1,29 +1,40 @@
 let todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+const savedList = JSON.parse(localStorage.getItem("saved-items"));
 
-const createTodo = function () {
-  const todoList = document.querySelector("#todo-list");
+const createTodo = function (savedList) {
   const newLi = document.createElement("li");
   const newSpan = document.createElement("span");
   const newBtn = document.createElement("button");
 
   newBtn.addEventListener("click", () => {
     newLi.classList.toggle("complete");
+    savedItemsFn();
   });
 
   newLi.addEventListener("dbclick", () => {
     newLi.remove();
   });
+  if (savedList) {
+    newSpan.textContent = savedList.contents;
+    if (savedList.complete) {
+      newLi.classList.toggle("complete");
+    }
+  } else {
+    newSpan.textContent = todoInput.value;
+  }
 
-  newSpan.textContent = todoInput.value;
   newLi.appendChild(newBtn);
   newLi.appendChild(newSpan);
   todoList.appendChild(newLi);
   todoInput.value = "";
+  console.log(savedList.complete);
 };
 
 const keyCodeCheck = function (e) {
   if (e.key === "Enter" && todoInput.value) {
     createTodo();
+    savedItemsFn();
   }
 };
 
@@ -33,3 +44,21 @@ const deleteAll = function () {
     liList[i].remove();
   }
 };
+
+const savedItemsFn = function () {
+  const savedItems = [];
+  for (let i = 0; i < todoList.children.length; i++) {
+    const todoObj = {
+      contents: todoList.children[i].innerText,
+      complete: todoList.children[i].classList.contains("complete"),
+    };
+    savedItems.push(todoObj);
+  }
+  localStorage.setItem("saved-items", JSON.stringify(savedItems));
+};
+
+if (savedList) {
+  for (let i = 0; i < savedList.length; i++) {
+    createTodo(savedList[i]);
+  }
+}
